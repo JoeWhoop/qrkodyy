fe7b339a5d3804e71b9e3d24022379a2525e3cc9
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2023-10-11 16:38:28 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,24 @@
+// JavaScript to handle button click and redirection
+document.getElementById("goButton").addEventListener("click", function () {
+    var selectedMechanic = document.getElementById("mechanicDropdown").value;
+    var selectedStation = document.getElementById("stationDropdown").value;
+
+    // Define the base URL for Lukasz Chojecki
+    var baseRedirectURL = "https://web.miniextensions.com/Bwk6PtCv1a4zbRONadUU?prefill_Mechanic=LUKASZ_CHOJECKI";
+
+    // Define redirection URLs for different stations for Lukasz Chojecki
+    var redirectionURLs = {
+        "D0": baseRedirectURL + "&prefill_Station=D0",
+        "D1": baseRedirectURL + "&prefill_Station=D1",
+        "D2": baseRedirectURL + "&prefill_Station=D2",
+        "D3": baseRedirectURL + "&prefill_Station=D3",
+        "D4": baseRedirectURL + "&prefill_Station=D4"
+    };
+
+    // Check if the selected station exists in the redirection URLs
+    if (redirectionURLs[selectedStation]) {
+        window.location.href = redirectionURLs[selectedStation];
+    } else {
+        alert("Please select both a mechanic and a station before clicking Go.");
+    }
+});
