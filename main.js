cf66a5c9721257af2ca4d14604d4034a280c51eb
JoeWhoop <147258204+JoeWhoop@users.noreply.github.com>
2023-10-11 22:05:19 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-// JavaScript to handle button click and redirection
 document.getElementById("goButton").addEventListener("click", function () {
     var selectedMechanic = document.getElementById("mechanicDropdown").value;
     var selectedStation = document.getElementById("stationDropdown").value;
 
-    // Convert mechanic and station values to uppercase and replace spaces with underscores
     selectedMechanic = selectedMechanic.toUpperCase().replace(" ", "_");
     selectedStation = selectedStation.toUpperCase().replace(" ", "_");
 
-    // Define the base URL for the selected mechanic
     var baseRedirectURL = "https://web.miniextensions.com/Bwk6PtCv1a4zbRONadUU?prefill_Mechanic=" + selectedMechanic;
 
-    // Define redirection URLs for different stations
     var redirectionURLs = {
         "D0": baseRedirectURL + "&prefill_Station=" + selectedStation,
         "D1": baseRedirectURL + "&prefill_Station=" + selectedStation,
@@ -19,7 +15,6 @@ document.getElementById("goButton").addEventListener("click", function () {
         "D4": baseRedirectURL + "&prefill_Station=" + selectedStation
     };
 
-    // Check if the selected station exists in the redirection URLs
     if (redirectionURLs[selectedStation]) {
         window.location.href = redirectionURLs[selectedStation];
     } else {
